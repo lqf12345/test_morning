8c77e26ad02c8ae757feeb599f8f6c2c3b244fb0
craker <57425385+lqf12345@users.noreply.github.com>
2022-08-22 22:02:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ client = WeChatClient(app_id, app_secret)
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
 low, low = get_low()
-high, high = get_low()
+high, high = get_high()
 data = {"low":{"value":low, "color":get_random_color()}, "high":{"value":high, "color":get_random_color()}, "today":{"value":get_today(), "color":get_random_color()},"weather":{"value":wea, "color":get_random_color()},"temperature":{"value":temperature, "color":get_random_color()},"love_days":{"value":get_count(), "color":get_random_color()},"birthday_left":{"value":get_birthday(), "color":get_random_color()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
